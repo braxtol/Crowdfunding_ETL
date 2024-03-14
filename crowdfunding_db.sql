c7765e7928c9aa39623892230d357b80a1f09573
James Montgomery <145395275+braxtol@users.noreply.github.com>
2024-03-14 08:43:18 -0700

    another commit

---
+++ crowdfunding_db.sql
@@ -0,0 +1,23 @@
+SELECT * 
+FROM contacts;
+
+SELECT * 
+FROM category;
+
+SELECT * 
+FROM subcategory;
+
+SELECT * 
+FROM campaign;
+
+SELECT * 
+FROM contacts
+WHERE last_name LIKE 'La%';
+
+SELECT * 
+FROM campaign
+WHERE outcome LIKE 'li%';
+
+SELECT * 
+FROM campaign
+WHERE currency LIKE 'D%';
